26aa012efbd23b89a46408370bb24e17bf3c8f64
MiguelA-Zurita <mzurita@nigul.cide.es>
2025-09-29 18:26:04 +0200

    Added README + comments

--- u1ex01/src/main/java/org/example/Main.java
+++ u1ex01/src/main/java/org/example/Main.java
@@ -1,30 +1,34 @@
 package org.example;
 
-import java.io.*;
+import java.io.*; //import libraries
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); //Create a Scanner to read user's input
         System.out.print("Please enter the path of origin of the file you want to copy: ");
-        String origin = input.nextLine();
+        String origin = input.nextLine(); // Read user's input
         System.out.print("Please enter the path of the file you want to paste to: ");
-        String destiny = input.nextLine();
-        if (origin.endsWith(".xml") || origin.endsWith(".txt")) {
+        String destiny = input.nextLine(); // Read user's input
+        if (origin.endsWith(".xml") || origin.endsWith(".txt")) { //if the file is a xml or txt, work with FileReader and FileWriter
             try (FileReader originFile = new FileReader(origin);
-                 FileWriter destinyFile = new FileWriter(destiny)) {
-                while(originFile.ready()) {
+                 FileWriter destinyFile = new FileWriter(destiny)) { //try catch in case any paths are not valid
+                while(originFile.ready()) { //While the file is ready to be red, read
                     destinyFile.write(originFile.read());
                 }
             } catch (Exception e) {
-                System.out.println("Origin file not found");
+                System.out.println("Origin file not found or destiny path not found"); //Message to let know there's been an error
+            } finally {
+                System.out.println("File copied successfully");
             }
         } else {
             try(FileInputStream originFile = new FileInputStream(origin);
-            FileOutputStream destinyFile = new FileOutputStream(destiny)) {
-                destinyFile.write(originFile.readAllBytes());
+            FileOutputStream destinyFile = new FileOutputStream(destiny)) { //try catch in case any paths are invalid
+                destinyFile.write(originFile.readAllBytes()); //Reads all bytes and after writes the given byte array
             } catch (Exception e) {
-                System.out.println("Origin file not found");
+                System.out.println("Origin file not found or destiny path not found"); //Message to let know there's been an error
+            } finally{
+                System.out.println("File copied successfully");
             }
         }
     }
